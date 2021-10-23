dc265216e49677c2dbc68abca433c6d2f4775ddd
Dmitry Bychenko <Zwerg.CopperNickel@gmail.com>
2021-10-24 01:15:26 +0300

    Laplace

--- Simple.Statistics/Distributions/Library/Simple.Statistics.Distributions.Library.Laplace.cs
+++ Simple.Statistics/Distributions/Library/Simple.Statistics.Distributions.Library.Laplace.cs
@@ -5,26 +5,30 @@ namespace Simple.Statistics.Distributions.Library {
   //-------------------------------------------------------------------------------------------------------------------
   //
   /// <summary>
-  /// Rayleigh Distribution
+  /// Laplace Distribution
   /// </summary>
-  /// <seealso cref="https://en.wikipedia.org/wiki/Rayleigh_distribution"/>
+  /// <seealso cref="https://en.wikipedia.org/wiki/Laplace_distribution"/>
   //
   //-------------------------------------------------------------------------------------------------------------------
 
-  public sealed class RayleighDistribution : ContinuousDistribution {
+  public sealed class LaplaceDistribution : ContinuousDistribution {
     #region Create
 
     /// <summary>
     /// Standard constructor
     /// </summary>
-    /// <param name="sigma">Rate</param>
-    public RayleighDistribution(double sigma) {
-      if (sigma <= 0 || !double.IsFinite(sigma))
-        throw new ArgumentOutOfRangeException(nameof(sigma), "sigma value must be positive");
+    /// <param name="b">Rate</param>
+    public LaplaceDistribution(double mu, double b) {
+      if (!double.IsFinite(mu))
+        throw new ArgumentOutOfRangeException(nameof(mu), "mu must be finite");
 
-      Sigma = sigma;
-      m_Mean = sigma * Math.Sqrt(Math.PI / 2.0);
-      m_Variance = (4 - Math.PI) / 2 * sigma * sigma;
+      if (b <= 0 || !double.IsFinite(b))
+        throw new ArgumentOutOfRangeException(nameof(b), "b value must be positive");
+
+      Mu = mu;
+      B = b;
+      m_Mean = mu;
+      m_Variance = 2 * b * b;
     }
 
     #endregion Create
@@ -32,14 +36,19 @@ namespace Simple.Statistics.Distributions.Library {
     #region Public
 
     /// <summary>
-    /// Sigma parameter 
+    /// Mu parameter 
     /// </summary>
-    public double Sigma { get; }
+    public double Mu { get; }
+
+    /// <summary>
+    /// B parameter 
+    /// </summary>
+    public double B { get; }
 
     /// <summary>
     /// To String 
     /// </summary>
-    public override string ToString() => $"Rayleigh distribution with sigma = {Sigma}";
+    public override string ToString() => $"Laplace distribution with mu = {Mu}, b = {B}";
 
     #endregion Public
 
@@ -49,15 +58,15 @@ namespace Simple.Statistics.Distributions.Library {
     /// Cumulative Density Function
     /// </summary>
     /// <see cref="https://en.wikipedia.org/wiki/Cumulative_distribution_function"/>
-    public override double Cdf(double x) =>
-      1 - Math.Exp(-x * x / 2 / Sigma / Sigma);
+    public override double Cdf(double x) => x <= Mu
+      ? Math.Exp((x - Mu) / B) / 2
+      : 1 - Math.Exp((Mu - x) / B) / 2;
 
     /// <summary>
     /// Probability Distribution Function
     /// </summary>
     /// <see cref="https://en.wikipedia.org/wiki/Probability_density_function"/>
-    public override double Pdf(double x) =>
-      x / Sigma / Sigma * Math.Exp(-x * x / 2 / Sigma / Sigma);
+    public override double Pdf(double x) => Math.Exp(- Math.Abs(x - Mu) / B) / 2 / B;
 
     /// <summary>
     /// Quantile Distribution Function
@@ -65,13 +74,15 @@ namespace Simple.Statistics.Distributions.Library {
     /// <see cref="https://en.wikipedia.org/wiki/Quantile_function"/>
     public override double Qdf(double x) {
       if (x == 0)
-        return 0.0;
+        return double.NegativeInfinity;
       if (x == 1)
         return double.PositiveInfinity;
       if (x < 0 || x > 1)
         throw new ArgumentOutOfRangeException(nameof(x));
 
-      return Sigma * Math.Sqrt(-2 * Math.Log(1 - x));
+      return x <= 0.5
+        ? Mu + B * Math.Log(2 * x)
+        : Mu - B * Math.Log(2 - 2 * x);
     }
 
     #endregion IContinuousDistribution
